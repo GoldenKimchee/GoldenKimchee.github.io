503aae89d5c9b99dab4560d4e13f966b30ca1d37
Rachel Kim <rachelkim8084@gmail.com>
2022-04-01 16:49:30 -0700

    refactor

--- review/index.js
+++ review/index.js
@@ -29,35 +29,13 @@ function toggleTab(tab) {
   }
 }
 
-function loadHome() {
+function loadPage(page) {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.querySelector("section").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "pages/home.txt", true);
-  xhttp.send();
-}
-
-function loadProjects() {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.querySelector("section").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "pages/projects.txt", true);
-  xhttp.send();
-}
-
-function loadContacts() {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.querySelector("section").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "pages/contacts.txt", true);
+  xhttp.open("GET", `pages/${page}`, true);
   xhttp.send();
 }
